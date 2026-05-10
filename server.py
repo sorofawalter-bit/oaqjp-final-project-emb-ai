c7352ad63b5ded12ec21fde8055ee2143c58c34a
sorofawalter-bit <sorofawalter@gmail.com>
2026-05-10 02:20:01 -0400

    Initial commit: Emotion Detector App

---
+++ server.py
@@ -0,0 +1,45 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask pramework package : TODO
+# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+#Initiate the flask app : TODO
+app = Flask("Sentiment Analyzer")
+
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    '''
+    Retrieve the text to analyze from the request arguments,
+    process it through the sentiment analyzer, and return the result.
+    '''
+	# Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+	# Pass the text to the sentiment_analyzer function and store the response
+    response = sentiment_analyzer(text_to_analyze)
+
+	# Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+		# Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+ #   else:
+        # Return a formatted string with the sentiment label and score
+    return f"The given text has been identified as {label} with a score of {score}."
+
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
